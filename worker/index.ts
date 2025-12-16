14e82bf49efe0f2af2b2b1502180f616a52c6182
Ivan Melnyk <github@ivanmelnyk.com>
2025-12-16 17:00:52 +0000

    added github workflow

--- worker/index.ts
+++ worker/index.ts
@@ -1,15 +1,20 @@
 import { Hono } from 'hono'
 import { basicAuth } from 'hono/basic-auth'
 
-const app = new Hono()
+type Bindings = {
+  USERNAME: string
+  PASSWORD: string
+}
 
-app.use(
-  "/admin/*",
-  basicAuth({
-    username: "admin",
-    password: "secret",
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.use('/admin/*', async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
   })
-)
+  return auth(c, next)
+})
 
 app.get('/admin', (c) => {
   return c.text('You are Authorized!')
